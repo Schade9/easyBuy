a1b4ca85dbc1eb965c5f5fc41acafd53432e14a4
Schade9 <s.charlesderrick9@gmail.com>
2022-03-11 19:38:29 +0300

    Auth complete

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/categories', [HomeController::class, 'categories'])->name('categories');
+
+Route::get('/admin', [HomeController::class, 'admin'])->name('admin')->middleware('is_admin');
